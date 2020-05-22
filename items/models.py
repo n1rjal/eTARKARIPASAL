c7bb4fc4af22c7a0ce05ba4d7f3759c02daa5047
n1rjal <nirjalpaudel54312@gmail.com>
2020-05-23 00:10:35 +0545

    deleted collected static

--- items/models.py
+++ items/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
